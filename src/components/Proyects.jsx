1acec0cadab29c381f819be4a4ef93bfa6ba3d95
Jeriel <jerielweb@outlook.com>
2025-10-19 16:06:28 -0600

    Add Proyect Section

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,30 +1,60 @@
 // src/components/ProjectCard.jsx
 import proyectsData from "./../data/proyects"
-
-
 export default function ProjectCard() {
   return (
-    <section className='flex text-purple-100 flex-col gap-5'>
-      <h2>Proyectoes mas Recientes</h2>
+    <section id="proyects" className='flex min-h-[650px] text-purple-100 flex-col gap-5 justify-center w-full items-center'>
+      <h2 className="text-center font-extrabold text-5xl">Proyectos más Recientes</h2>
       {proyectsData.map ((data, index) => (
-        <article key={index} className='flex rounded-3xl overflow-hidden flex-row lg:w-5xl lg:h-64 border-b-purple-50 border justify-between items-center'>
-          <div className="max-w-lg flex lg:flex-col justify-evenly h-full">
-              <h2>{data.title}</h2>
-            <div>
-              {data.description instanceof Array ? (
-                          data.description.map((desc, i) => (
-                              <p key={i}>{desc}</p>
-                          ))
-                      ) : (
-                          <p className="max-w-[400px]">{data.description}</p>
-                      )}
-            </div>
-            <div>
-
+        <article key={index} className='flex gap-5 rounded-2xl m-8 overflow-hidden lg:pr-0 lg:pl-0 flex-col-reverse xl:flex-row lg:max-w-6xl xl:h-96 border-3 justify-between items-center hover:scale-103 transition duration-200 ease-out'>
+          <div className="xl:max-w-lg w-full xl:w-auto flex flex-col h-full p-5">
+            <div className="flex h-full flex-col justify-between gap-5">
+                <h2
+                className="text-4xl font-bold"
+                >{data.title}</h2>
+              <div>
+                {/* Lógica para descripción: Correcta */}
+                {data.description instanceof Array ? (
+                            data.description.map((desc, i) => (
+                                <p key={i} >{desc}</p>
+                            ))
+                        ) : (
+                            <p
+                            className="max-w-[400px] text-[1.3rem] font-semibold"
+                            >{data.description}</p>
+                        )}
+              </div>
+              <div>
+                <h2
+                className="text-3xl font-bold"
+                >Stacks</h2>
+                <div className="flex flex-row w-max gap-2">
+                {data.tags instanceof Array && data.frameworks instanceof Array && data.tags.length === data.frameworks.length ? (
+                  data.tags.map((imgSrc, p) => (
+                    <img
+                      key={p}
+                      className="size-12 aspect-square pointer-events-none rounded"
+                      alt={data.frameworks[p]}
+                      itemType="image/svg+xml"
+                      src={imgSrc}
+                    />
+                  ))
+                ) : (
+                  <img
+                    itemType="image/svg+xml"
+                    src={data.tags}
+                    alt={`Iconos de tecnologías para ${data.title}`}
+                  />
+                )}
+                </div>
+              </div>
+              <div className="flex lg:w-full lg:flex-row gap-5 font-semibold text-center">
+                <a className="text-[20px] size-fit bg-purple-700 py-2 px-5 rounded-[10px]" target="_blank" href={data.liveLink}>Ver Demo</a>
+                <a className="text-[20px] size-fit bg-purple-700 py-2 px-5 rounded-[10px]" target="_blank" href={data.githubLink}>Ver Codigo</a>
+              </div>
             </div>
           </div>
-          <div>
-            <img src={data.imgSrc} alt={data.title} className=" max-w-[512px]"/>
+          <div className="flex lg:p-0 mask-clip-border">
+            <img src={data.imgSrc} alt={data.imgScreen} className="max-h-96 h-auto object-contain aspect-video rounded-2xl mask-clip-border"/>
           </div>
         </article>
       ))}
--- src/data/proyects.js
+++ src/data/proyects.js
@@ -1,16 +1,37 @@
-import oboeEdits from "./../assets/oboe-editsWeb.webp"
+
+//Screen Shots
+import oboeEdits from "./../assets/758shots_so.png"
+import calc from "./../assets/678shots_so.png"
+
+// Tags
+import Astro from "./../assets/astro.svg"
+import ReactJS from "./../assets/react.js.svg"
+import CSS from "./../assets/css.svg"
+import JS from "./../assets/js.svg"
+import HTML from "./../assets/html.svg"
 
 
 export const projects = [
   {
     title: 'Oboe Edits',
-    description: 'Una web Loading Page donde se especializa ofrecer servicios de edición de alta calidad.',
+    description: 'Un sitio Loading Page donde se especializa ofrecer servicios de edición de alta calidad.',
     imgSrc: oboeEdits.src,
-    tags: ['React', 'Node.js', 'Tailwind', 'Stripe'],
-    githubLink: 'https://github.com/tu-usuario/ecommerce',
-    liveLink: 'https://mi-ecommerce.com'
+    imgScreen: 'Captura de pantalla del sitio Oboe Edits',
+    frameworks: ['ReactJS', 'JavaScript', 'CSS'],
+    tags: [ReactJS.src, JS.src, CSS.src],
+    githubLink: 'https://github.com/jerielweb/Oboe-Edits',
+    liveLink: 'https://oboe-edits.vercel.app/'
   },
-  // ... añade más proyectos aquí
+  {
+    title: 'Calculadora',
+    description: 'Un proyecto personal hecho destacadamente para el aprendizaje, fue creado durante el curso de JavaScript.',
+    imgSrc: calc.src,
+    imgScreen: 'Captura de pantalla De la calculadora',
+    frameworks: ['HTML', 'CSS', 'JavaScript'],
+    tags: [JS.src, HTML.src, CSS.src],
+    githubLink: 'https://github.com/jerielweb/calculadora',
+    liveLink: 'https://jerielweb.github.io/calculadora/'
+  }
 ];
 
 export default projects
