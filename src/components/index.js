34d40d00a8850f4dfab959007d031458045311d9
Jeriel Beckford <jerielweb@outlook.es>
2025-10-25 15:39:02 -0600

    More clean code

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import HeroComponent from './Hero.astro';
+import ProyectsComponent from './Proyects.jsx'
+import ContactFormComponent from './contactform.astro';
+import AboutComponent from './About.astro'
+import SkilsComponent from './skils.jsx'
+
+export const Hero = HeroComponent
+export const Layout = LayoutComponent
+export const Proyects = ProyectsComponent
+export const ContactForm = ContactFormComponen
+export const About = AboutComponent
+export const Skils = SkilsComponent
