92e26eaf3341e295a4af72cd2d2c5a812c51db70
Jeriel Beckford <jerielweb@outlook.es>
2025-10-25 15:25:30 -0600

    Fix Errors

--- src/data/Data.Skils.js
+++ src/data/Data.Skils.js
@@ -1,4 +1,4 @@
-import { JS, HTML, Astro, ReactJS, CSS, Tailwind, VScode, node, vite, npm, pnpm, cursorAI, git, GitHub, bash, yarn } from "./../"
+import { JS, HTML, Astro, ReactJS, CSS, Tailwind, VScode, node, vite, npm, pnpm, cursorAI, git, GitHub, bash, yarn } from "./../assets/index.js"
 
 export const skilsData = [
     {
