5d80ea386324c9d69c1a0f6ac33841839b2c8c2a
Jeriel Beckford <jerielweb@outlook.es>
2025-10-25 13:04:32 -0600

    sEGUNDO INTENTO

--- src/data/index.js
+++ src/data/index.js
@@ -1,43 +1,48 @@
-/*Importar*/
-import JS from "./../assets/icons/js.svg"
-import HTML from "./../assets/icons/html.svg"
-import Astro from "./../assets/icons/astro.svg"
-import ReactJS from "./../assets/icons/react.js.svg"
-import CSS from "./../assets/icons/css.svg"
-import Tailwind from "./../assets/icons/tailwind.svg"
-import VScode from "./../assets/icons/VScode.svg"
-import node from "./../assets/icons/nodeJS.svg"
-import vite from "./../assets/icons/vite.svg"
-import npm from "./../assets/icons/npm.svg"
-import pnpm from "./../assets/icons/pnpm-3.svg"
-import cursorAI from "./../assets/icons/cursorai.svg"
-import git from "./../assets/icons/git.svg"
-import GitHub from "./../assets/icons/github.svg"
-import bash from "./../assets/icons/terminal.svg"
-import yarn from "./../assets/icons/yarn-svgrepo-com.svg"
+/* Importar - Astro/Vite procesa estas rutas y las convierte en objetos. */
+// Tags (usando 'Icon' para el nombre local de la importación)
+import jsIcon from "./../assets/icons/js.svg";
+import htmlIcon from "./../assets/icons/html.svg";
+import astroIcon from "./../assets/icons/astro.svg";
+import reactJSIcon from "./../assets/icons/react.js.svg";
+import cssIcon from "./../../assets/icons/css.svg"; // <-- ¡Asegúrate de que esta ruta es correcta!
+import tailwindIcon from "./../assets/icons/tailwind.svg";
+import vscodeIcon from "./../assets/icons/VScode.svg";
+import nodeIcon from "./../assets/icons/nodeJS.svg";
+import viteIcon from "./../assets/icons/vite.svg";
+import npmIcon from "./../assets/icons/npm.svg";
+import pnpmIcon from "./../assets/icons/pnpm-3.svg";
+import cursorAIIcon from "./../assets/icons/cursorai.svg";
+import gitIcon from "./../assets/icons/git.svg";
+import gitHubIcon from "./../assets/icons/github.svg";
+import bashIcon from "./../assets/icons/terminal.svg";
+import yarnIcon from "./../assets/icons/yarn-svgrepo-com.svg";
+
+// Screenshots (usando 'Img' para el nombre local de la importación)
+import oboeEditsImg from "./../assets/758shots_so.png";
+import calcImg from "./../assets/678shots_so.png";
+import portfolioImg from "./../assets/743shots_so.png";
 
 
-/*Exportar*/
+/* Exportar - Exportamos los OBJETOS de imagen que contienen el .src */
+// Tags
+export const JS = jsIcon;
+export const HTML = htmlIcon;
+export const Astro = astroIcon;
+export const ReactJS = reactJSIcon; // Se corrigió el nombre de la variable importada
+export const CSS = cssIcon;
+export const Tailwind = tailwindIcon;
+export const VScode = vscodeIcon;
+export const node = nodeIcon;
+export const vite = viteIcon;
+export const npm = npmIcon;
+export const pnpm = pnpmIcon;
+export const cursorAI = cursorAIIcon;
+export const git = gitIcon;
+export const GitHub = gitHubIcon;
+export const bash = bashIcon;
+export const yarn = yarnIcon;
 
-//Tags
-export const JS = "./../assets/icons/js.svg"
-export const HTML = "./../assets/icons/html.svg"
-export const Astro = "./../assets/icons/astro.svg"
-export const ReactJS = "./../assets/icons/react.js.svg"
-export const CSS = "./../assets/icons/css.svg"
-export const Tailwind = "./../assets/icons/tailwind.svg"
-export const VScode = "./../assets/icons/VScode.svg"
-export const node = "./../assets/icons/nodeJS.svg"
-export const vite = "./../assets/icons/vite.svg"
-export const npm = "./../assets/icons/npm.svg"
-export const pnpm = "./../assets/icons/pnpm-3.svg"
-export const cursorAI = "./../assets/icons/cursorai.svg"
-export const git = "./../assets/icons/git.svg"
-export const GitHub = "./../assets/icons/github.svg"
-export const bash = "./../assets/icons/terminal.svg"
-export const yarn = "./../assets/icons/yarn-svgrepo-com.svg"
-
-//ScreenShots
-export const oboeEdits = "./../assets/758shots_so.png"
-export const calc = "./../assets/678shots_so.png"
-export const portfolio = "./../assets/743shots_so.png"
+// Screenshots
+export const oboeEdits = oboeEditsImg;
+export const calc = calcImg;
+export const portfolio = portfolioImg;
