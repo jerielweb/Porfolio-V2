710801b001328181cfff68076a1d73b95ebf4d37
Jeriel Beckford <jerielweb@outlook.es>
2025-10-25 15:43:56 -0600

    More fixes

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@ import ProyectsComponent from './Proyects.jsx'
 import ContactFormComponent from './contactform.astro';
 import AboutComponent from './About.astro'
 import SkilsComponent from './skils.jsx'
+import HeaderComponents from './Header.astro'
 
 export const Hero = HeroComponent
 export const Proyects = ProyectsComponent
 export const ContactForm = ContactFormComponen
 export const About = AboutComponent
 export const Skils = SkilsComponent
+export const Header = HeaderComponents
