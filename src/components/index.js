95ea3e7fb209a485a52d1809732ea64806209f13
Jeriel Beckford <jerielweb@outlook.es>
2025-10-25 15:39:55 -0600

    fix layout

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,6 @@ import AboutComponent from './About.astro'
 import SkilsComponent from './skils.jsx'
 
 export const Hero = HeroComponent
-export const Layout = LayoutComponent
 export const Proyects = ProyectsComponent
 export const ContactForm = ContactFormComponen
 export const About = AboutComponent
