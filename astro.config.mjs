8a84c922e040815932a68e9bafbec86cf8958ab9
Jeriel <jerielweb@outlook.com>
2025-11-05 17:14:43 -0600

    feat: add sharp dependency and configure passthrough image service in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -11,6 +11,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   image: {
-    domains: ["astro.build"],
+    service: passthroughImageService(),
   },
 })
